f9afed0ebe3c98854d00b3879d6c4cfa804e3673
Suin Kim <contact@suinkim.me>
2025-05-15 22:39:45 +0900

    프로필 바 여백 수정

--- src/components/common/profile.tsx
+++ src/components/common/profile.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export function Profile() {
   return (
-    <div className="flex items-center gap-2 px-6 py-12">
+    <div className="flex items-center gap-2 px-6 py-9">
       <Avatar className="size-15">
         <AvatarImage src="https://github.com/suinkimme.png" />
         <AvatarFallback>SU</AvatarFallback>
