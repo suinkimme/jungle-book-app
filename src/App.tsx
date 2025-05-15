79213f62e932901a2a435cd1578901f62c51fcdb
Suin Kim <contact@suinkim.me>
2025-05-15 18:39:05 +0900

    Shadcn/ui 사용 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
