f3972e299678bb92971a5a6e9d35835b044632c9
Suin Kim <contact@suinkim.me>
2025-05-16 00:14:25 +0900

    상단 룸 넘버 컨테이너 고정

--- src/components/common/room-number-container.tsx
+++ src/components/common/room-number-container.tsx
@@ -3,7 +3,7 @@ import { Badge } from '@/components/ui/badge';
 
 export function RoomNumberContainer() {
   return (
-    <div className="w-full px-6 py-7 flex justify-start items-center gap-3 bg-secondary">
+    <div className="sticky top-0 z-10 w-full px-6 py-7 flex justify-start items-center gap-3 bg-secondary">
       <CubeIcon className="w-6 h-6" />
       <h1 className="text-2xl font-bold">303 코칭실</h1>
       <Badge variant="outline">예약가능</Badge>
