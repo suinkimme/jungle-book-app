35eb9f9d92cfe4916f5af174c26735107fd8847e
Suin Kim <contact@suinkim.me>
2025-05-16 00:01:07 +0900

    날짜 함수 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,3 +6,25 @@ export const formatDateWithWeekday = (date: Date) => {
   const weekday = date.getDay();
   return `${month}월 ${day}일 (${WEEKDAYS[weekday]})`;
 };
+
+export const generateTimeSlots = (startHour = 10, endHour = 22) => {
+  const slots = [];
+  for (let hour = startHour; hour < endHour; hour++) {
+    const start = formatHour(hour);
+    const end = formatHour(hour + 1);
+    slots.push(`${start} - ${end}`);
+  }
+  return slots;
+};
+
+export const formatHour = (hour: number) => {
+  if (hour === 0) {
+    return '오전 12:00';
+  } else if (hour < 12) {
+    return `오전 ${hour}:00`;
+  } else if (hour === 12) {
+    return '오후 12:00';
+  } else {
+    return `오후 ${hour - 12}:00`;
+  }
+};
