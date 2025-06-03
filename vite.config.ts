a540cd18e4cbe8e8a982ef020d69b119ac965435
Suin Kim <contact@suinkim.me>
2025-06-03 14:38:16 +0900

    feat: vite proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://coachroom.duckdns.org',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
