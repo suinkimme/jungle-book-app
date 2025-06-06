5443a5673a7b40d33acc2a627589187925702a47
Suin Kim <contact@suinkim.me>
2025-06-06 10:29:48 +0900

    feat: 예약 버튼에 debounce 추가

--- src/hooks/useReservation.ts
+++ src/hooks/useReservation.ts
@@ -1,9 +1,10 @@
 import { toast } from 'sonner';
+import debounce from 'lodash/debounce';
 import { reservationRoom, cancelReservation } from '@/api';
 import { isBefore10AM, isAfter4AM } from '@/utils';
 
 export const useReservation = () => {
-  const handleReservation = async (roomId: number) => {
+  const handleReservation = debounce(async (roomId: number) => {
     if (isBefore10AM(new Date())) {
       toast.error('예약 시간은 10시 이후부터 가능합니다.');
       return;
@@ -20,9 +21,9 @@ export const useReservation = () => {
     } catch (error) {
       toast.error('예약에 실패했습니다.');
     }
-  };
+  }, 400);
 
-  const handleCancel = async () => {
+  const handleCancel = debounce(async () => {
     try {
       const response = await cancelReservation();
       toast.error(response.message);
@@ -30,6 +31,7 @@ export const useReservation = () => {
       console.log(error);
       toast.error('예약 취소에 실패했습니다.');
     }
-  };
+  }, 400);
+
   return { handleReservation, handleCancel };
 };
