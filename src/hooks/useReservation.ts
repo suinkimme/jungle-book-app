7d031a174ff5e942e7346cedecc8a9c8ceee5c73
Suin Kim <contact@suinkim.me>
2025-06-06 13:30:03 +0900

    fix: 서버에 요청 시 리스트 새로 불러오도록 수정함

--- src/hooks/useReservation.ts
+++ src/hooks/useReservation.ts
@@ -1,9 +1,13 @@
 import { toast } from 'sonner';
 import debounce from 'lodash/debounce';
+
 import { reservationRoom, cancelReservation } from '@/api';
+import { useRooms } from '@/hooks';
 import { isBefore10AM, isAfter4AM } from '@/utils';
 
 export const useReservation = () => {
+  const { fetchRoomList } = useRooms();
+
   const handleReservation = debounce(async (roomId: number) => {
     if (isBefore10AM(new Date())) {
       toast.error('예약 시간은 10시 이후부터 가능합니다.');
@@ -18,6 +22,8 @@ export const useReservation = () => {
     try {
       const response = await reservationRoom(roomId);
       toast.success(response.message);
+      // TODO: 낙관적 UI update로 분리해야함
+      fetchRoomList();
     } catch (error) {
       toast.error('예약에 실패했습니다.');
     }
@@ -27,6 +33,8 @@ export const useReservation = () => {
     try {
       const response = await cancelReservation();
       toast.success(response.message);
+      // TODO: 낙관적 UI update로 분리해야함
+      fetchRoomList();
     } catch (error) {
       console.log(error);
       toast.error('예약 취소에 실패했습니다.');
